dcf40449da458d7d9d626090092f8ad551e3947c
jill-tan <sunny90989@gmail.com>
2024-03-22 16:12:02 +0800

    add_base_test

--- test_case/test_logout.py
+++ test_case/test_logout.py
@@ -4,10 +4,11 @@ from page.page_manager import(
   main_page,
 )
 from test_data.yaml_data import YamlData
+from test_case.test_base import BaseTest
 
-
-def test_act(yaml_data: YamlData):
-    pre_page.click_login()
-    login_page.login_act(yaml_data.email, yaml_data.pwd)
-    main_page.logout_act()
-    main_page.close()
+class TestCase(BaseTest):
+    def test_act(self, yaml_data: YamlData):
+        pre_page.click_login()
+        login_page.login_act(yaml_data.email, yaml_data.pwd)
+        main_page.logout_act()
+        main_page.close()
--- test_case/test_signup.py
+++ test_case/test_signup.py
@@ -3,9 +3,10 @@ from page.page_manager import(
   login_page,
 )
 from test_data.yaml_data import YamlData
+from test_case.test_base import BaseTest
 
-
-def test_act(yaml_data: YamlData):
-    pre_page.click_signup()
-    login_page.signup_act(yaml_data.email, yaml_data.pwd)
-    login_page.close()
+class TestCase(BaseTest):
+    def test_act(self, yaml_data: YamlData):
+        pre_page.click_signup()
+        login_page.signup_act(yaml_data.email, yaml_data.pwd)
+        login_page.close()
