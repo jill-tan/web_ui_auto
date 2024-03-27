50f4994051af15ae2b3ece367b944de8156cf8d2
jill-tan <sunny90989@gmail.com>
2024-03-27 17:56:57 +0800

    'deal_error'

--- page/base_page.py
+++ page/base_page.py
@@ -3,6 +3,7 @@ from common.selenium_act import SeleniumAct
 from page.pages_ele_loc import PagesEleLoc
 import time
 from common.const import PageName
+from selenium.common.exceptions import ElementClickInterceptedException
 
 class PageBase:
     page_name: PageName
@@ -13,23 +14,44 @@ class PageBase:
         if not page_name:
             page_name = self.page_name
 
-        return self.pages_ele_loc.get_ele_loc(
-            page_name,
-            ele_name
+        result = (
+            ele_name,
+            self.pages_ele_loc.get_ele_loc(
+                page_name,
+                ele_name
+            )
         )
+        return result
+
     def open_url(self, url, sleep_time=0):
         self.driver.open_url(url)
         self._wait_second(sleep_time)
+
     def ele_exist(self, *loc):
         return self.driver.ele_exist(*loc)
 
     def ele_click(self, *loc, sleep_time=0):
-        self.driver.ele_act(*loc, act=ELE_ACT.CLICK.value)
-        self._wait_second(sleep_time)
+        def _func(*loc):
+            self.driver.ele_act(*loc, act=ELE_ACT.CLICK.value)
+            self._wait_second(sleep_time)
+
+        self.ele_act(_func, *loc)
 
     def ele_send_str(self, *loc, send_str, sleep_time=0):
-        self.driver.ele_act(*loc, act=ELE_ACT.SEND_KEY.value, send_str=send_str)
-        self._wait_second(sleep_time)
+        def _func(*loc):
+            self.driver.ele_act(*loc, act=ELE_ACT.SEND_KEY.value, send_str=send_str)
+            self._wait_second(sleep_time)
+
+        self.ele_act(_func, *loc)
+
+    def ele_act(self, func, *loc):
+        ele_name, loc = loc
+        try:
+            func(*loc)
+        except ElementClickInterceptedException as e:
+            raise Exception(f"ElementClickInterceptedException -> {self.page_name} / {ele_name} \n------ \n{e.msg}")
+        except Exception as e:
+            raise
 
     def _wait_second(self, second):
         time.sleep(second)
--- test_case/test_base.py
+++ test_case/test_base.py
@@ -1,12 +1,18 @@
 from abc import abstractmethod
 from test_data.yaml_data import YamlData
 from common.const import TestResult
+from selenium.common.exceptions import ElementClickInterceptedException
 
 class BaseTest:
     test_result: TestResult
 
     def run_case(self, yaml_data: YamlData):
-        self.test_act(yaml_data)
+        try:
+            self.test_act(yaml_data)
+        except ElementClickInterceptedException as e:
+            print(e.msg)
+        except Exception as e:
+            print(e)
 
     @abstractmethod
     def test_act(self, yaml_data: YamlData):
